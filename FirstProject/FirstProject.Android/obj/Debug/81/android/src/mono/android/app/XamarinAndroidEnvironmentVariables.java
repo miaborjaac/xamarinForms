deac2449d030d23fe2da04d5464f4eb7660bd762
Miguel Angel Borja Acevedo <miaborjaac@unal.edu.co>
2019-04-21 20:13:16 -0500

    Day2 - Xaml styles

---
+++ FirstProject/FirstProject.Android/obj/Debug/81/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -0,0 +1,14 @@
+package mono.android.app;
+
+public class XamarinAndroidEnvironmentVariables
+{
+	// Variables are specified the in "name", "value" pairs
+	public static final String[] Variables = new String[] {
+		"MONO_LOG_LEVEL", "info",
+		"XAMARIN_BUILD_ID", "e4fcf4f5-e2ae-4a94-8bdd-129d3494938b",
+		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
+		"XA_TLS_PROVIDER", "btls",
+		"MONO_GC_PARAMS", "major=marksweep-conc",
+
+	};
+}
---
+++ FirstProject/FirstProject/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FirstProject
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        //private void button_clicked(object sender, eventargs e)
+        //{
+        //    if (!string.isnullorwhitespace(nameentry.text))
+        //    {
+        //        greetinglabel.text = $"hello {nameentry.text}, welcome to 10 days of xamarin.";
+        //    }
+        //    else
+        //    {
+        //        displayalert("error", "your name can't be empty", "oh right");
+        //        greetinglabel.text = $"";
+        //    }
+
+        void Save_Clicked(object sender, EventArgs e)
+        {
+            titleExperience.Text = $"";
+            contentExperience.Text = string.Empty;
+        }
+
+        private void EnabledOrDisabledBtn()
+        {
+            saveBtn.IsEnabled = false;
+            if(!string.IsNullOrWhiteSpace(titleExperience.Text) && !string.IsNullOrWhiteSpace(contentExperience.Text))
+            {
+                saveBtn.IsEnabled = true;
+            }
+        }
+
+        private void TitleExperience_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            EnabledOrDisabledBtn();
+        }
+
+        private void ContentExperience_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            EnabledOrDisabledBtn();
+        }
+    }
+}
